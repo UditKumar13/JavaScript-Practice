dc775fdc6b555869065ce69406c471a05c09afcb
Uditkumar13 <uditkumar2401@gmail.com>
2021-05-27 20:10:56 +0530

    dom

---
+++ js/sandbox.js
@@ -0,0 +1,15 @@
+const pa = document.querySelector('.para');
+console.log(pa);
+const pa2 = document.querySelector('body > div');
+console.log(pa2);
+
+const allDiv = document.querySelectorAll('div');
+console.log(allDiv);
+
+console.log(allDiv[1]);
+
+allDiv.forEach((uk)=>{
+console.log(uk);
+});
+    
+
